d0e304a7c49c5f4ceeaf23aa0f24d96f9a3a7f78
ajh12 <64850020+ajh12@users.noreply.github.com>
2025-12-26 20:19:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -348,8 +348,6 @@ if (!window.hasMainJsRun) {
             nameInput.value = "하포카";
             nameInput.disabled = true;
             pwContainer.classList.add('hidden');
-            const adminOpts = document.getElementById('admin-write-options');
-            if(adminOpts) adminOpts.classList.remove('hidden');
         }
 
         const editorHtml = document.getElementById('editorContentHtml');
